b6107b3070e2ec3bb896f4ae69719e3a3bac3632
Márcio Machado Ribeiro <jamesfoxbr@hotmail.com>
2023-08-30 10:34:20 -0300

    Changed the random generator

--- TextRPG/TextRPG.cpp
+++ TextRPG/TextRPG.cpp
@@ -1,24 +1,31 @@
-// programa para rolar dados em formato de rpg de mesa como por exemplo 1d20+5 ou 3d6 etc e dar o resultado
+// This code is a RPG tabletop dice roller. The user inputer a valid rpg roll format and the software will roll the results.
+// Example of valid rolls formates:
+// 1d6, 2d6, 1d8, 1d10, 1d12, 1d20, 3d6+4, 1d8-1, 10d6*3, 5d10/2, etc..
 
 #include <iostream>
+#include <random>
 
 using namespace std;
 
+// generate pseudo random numbers
 int Random(int sides)
 {
-	/* initialize random seed: */
-	static int n = 0;
-	srand(time(NULL) + n);
-	n++;
+	mt19937 mt(time(nullptr));
 
-	return rand() % sides + 1;
+	std::uniform_int_distribution<> dice{ 1, sides };
+
+	return dice(mt);
 }
 
+// get the user string input and transforms into usabe dice data. 
+// the user inpute need to be a valide rpg tabletop dice format like 1d10 or 1d8+3 etc
 int Roll()
 {
 	char ch;
 	cin.get(ch);                       
 
+	// gets all numbers bedore de "d" from the user string input
+	// example in 2d20+5 gets the 2
 	char num[10] = { 0 };            
 	int i = 0;
 	while (ch != 'd')
@@ -30,6 +37,8 @@ int Roll()
 
 	cin.get(ch);
 
+	// gets all numbers after de "d" from the user string input but before the modifier symbols.
+	// example in 1d20+5 gets the 20
 	char faces[10] = { 0 };          
 	int j = 0;
 	while (strchr("\n+-*/", ch) == 0)
@@ -39,6 +48,8 @@ int Roll()
 		j++;
 	}
 
+	// gets what is the modifier symbol
+	// example in 1d20+5 gets the +
 	char symbol[1];
 	char modifier[10] = { 0 };
 
@@ -53,6 +64,8 @@ int Roll()
 		cin.get(ch);
 	}
 	
+	//gets the modifier number after the symbol
+	// example in 1d20+5 gets the 5
 	int h = 0;
 	while (strchr("\n+-*/", ch) == 0)
 	{
@@ -61,6 +74,7 @@ int Roll()
 		h++;
 	}
 
+	// checks what symbol was used to make the right operation.
 	int total = 0;
 	switch (symbol[0])
 	{
@@ -99,11 +113,11 @@ int Roll()
 
 int main()
 {
-	cout << "Use o \"X\" da janela do console para fechar o programa: \n";
-	cout << "Escreva a rolagem de dado. Por exemplo 3d6 ou então 1d20+7: \n";
+	cout << "Use \"X\" on top right of the window to exit the program: \n";
+	cout << "Write some dice rool. Example 3d6 or 1d20+7: \n";
 	while (true)
 	{
-		cout << "Resultado: " << Roll();
+		cout << "Resul: " << Roll();
 		cout << endl;
 	}
 
